abbe8467c098aab74bcc0819c2251fd887d0d86d
DerekYunQue <1023883194@qq.com>
2021-04-14 22:15:40 +0800

    第一次提交

--- src/util/http.js
+++ src/util/http.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Toast } from 'vant'
 const http = axios.create({
   baseURL: 'https://m.maizuo.com',
   timeout: 10000,
@@ -6,4 +7,30 @@ const http = axios.create({
     'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"16181075882990680217485313","bc":"310100"}'
   }
 })
+
+http.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  Toast.loading({
+    message: '加载中...',
+    forbidClick: true,
+    overlay: true,
+    duration: 0
+  })
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+http.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  Toast.clear()
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
+
 export default http
